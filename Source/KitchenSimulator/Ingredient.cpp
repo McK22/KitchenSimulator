b2d8d8007e2fc3e6db0df51bdb164fbfc9c76217
McK <maciej.mpluta@gmail.com>
2025-12-14 20:05:49 +0100

    Move ingredient state logic to c++

--- Source/KitchenSimulator/Ingredient.cpp
+++ Source/KitchenSimulator/Ingredient.cpp
@@ -5,14 +5,16 @@ AIngredient::AIngredient()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetCollisionProfileName(TEXT("BlockAllDynamic"));
+	Mesh->SetSimulatePhysics(true);
+	SetRootComponent(Mesh);
 }
 
 // Called when the game starts or when spawned
 void AIngredient::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
@@ -21,6 +23,31 @@ void AIngredient::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+bool AIngredient::SetState(EIngredientState NewState)
+{
+	if (!IngredientData
+		|| !IngredientData->Meshes.Contains(NewState)
+		|| !Mesh->SetStaticMesh(IngredientData->Meshes[NewState]))
+	{
+		return false;
+	}
+	
+	State = NewState;
+	return true;
+}
+
+bool AIngredient::TryMakeTransition(EIngredientState NewState)
+{
+	if (!IngredientData || !IngredientData->Transitions[State].AllowedStates.Contains(NewState))
+	{
+		return false;
+	}
+
+	SetState(NewState);
+	return true;
+}
+
 
 void AIngredient::Cook_Implementation(float DeltaTime)
 {
--- Source/KitchenSimulator/Ingredient.h
+++ Source/KitchenSimulator/Ingredient.h
@@ -14,6 +14,9 @@ public:
 	// Sets default values for this actor's properties
 	AIngredient();
 
+	UFUNCTION(BlueprintCallable, Category = "State")
+	bool TryMakeTransition(EIngredientState NewState);
+
 	UFUNCTION(BlueprintNativeEvent, Category = "State")
 	void Cook(float DeltaTime);
 
@@ -23,12 +26,24 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "State")
+	EIngredientState State = EIngredientState::Whole;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Data", meta=(ExposeOnSpawn="true"))
+	UIngredientDataAsset* IngredientData;
+	
 	// Cooking time in seconds
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
 	float MaxCookingTime = 600.0f;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
 	float CookingTime = 0.0f;
+
+	UFUNCTION(BlueprintCallable, Category = "State")
+	bool SetState(EIngredientState NewState);
 
 public:	
 	// Called every frame
