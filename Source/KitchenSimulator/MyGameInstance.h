3e706d19c6137d1461c72b6f91522b54975ab2a8
McK <maciej.mpluta@gmail.com>
2026-01-28 22:25:47 +0100

    Create MyGameInstance class in cpp

---
+++ Source/KitchenSimulator/MyGameInstance.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RecipeDataAsset.h"
+#include "Engine/GameInstance.h"
+#include "Blueprint/UserWidget.h"
+#include "MyGameInstance.generated.h"
+
+UCLASS()
+class KITCHENSIMULATOR_API UMyGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<UUserWidget> ActiveMenu;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	URecipeDataAsset* SelectedRecipe;
+};
